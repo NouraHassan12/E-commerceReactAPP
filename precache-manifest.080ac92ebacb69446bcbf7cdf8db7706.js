ac6e4609863b3e148745fde63bfe2a2c7f84c7a5
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-07-05 23:31:23 -0700

    Updates

---
+++ precache-manifest.080ac92ebacb69446bcbf7cdf8db7706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f8aae28ccfa5bf03552ac0d4c326aae",
+    "url": "/E-commerceReactAPP/index.html"
+  },
+  {
+    "revision": "16bf420c99553ff69591",
+    "url": "/E-commerceReactAPP/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "212cedf4f1a4f8c1b858",
+    "url": "/E-commerceReactAPP/static/css/main.03701445.chunk.css"
+  },
+  {
+    "revision": "16bf420c99553ff69591",
+    "url": "/E-commerceReactAPP/static/js/2.30023be2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/E-commerceReactAPP/static/js/2.30023be2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212cedf4f1a4f8c1b858",
+    "url": "/E-commerceReactAPP/static/js/main.80052013.chunk.js"
+  },
+  {
+    "revision": "358768f8d6103b94b87c",
+    "url": "/E-commerceReactAPP/static/js/runtime-main.6184b96e.js"
+  }
+]);

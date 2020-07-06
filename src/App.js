f2c6438f32f6d8927f51020749fd120dd0ee2c3c
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-07-06 15:19:15 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {Switch,Route} from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './Components/Navbar';
 import Productlist from './Components/Productlist';
 import Details from './Components/Details';
 import Cart from './Components/Cart';
-import Default from './Components/Default';
+ import Default from './Components/Default';
 import Modal from './Components/Modal';
 
 
@@ -16,12 +16,12 @@ function App() {
 
 
      <Navbar />
-     <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <Switch >
        <Route  exact path="/" component={Productlist} />
        <Route path="/Details" component={Details} />
        <Route path="/Cart" component={Cart} />
        <Route  component={Default} />
-     </BrowserRouter>
+     </Switch>
      <Modal />
    </React.Fragment>
 
